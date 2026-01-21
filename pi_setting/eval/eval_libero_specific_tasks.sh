d7db8efa51a7aaafad86bfa401b032ebfbcefdbd
Your Name <your@email.com>
2026-01-21 20:16:09 +0000

    modify sh file for run libero

--- pi_setting/eval/eval_libero_specific_tasks.sh
+++ pi_setting/eval/eval_libero_specific_tasks.sh
@@ -1,22 +1,20 @@
 
-
-
 ALL_GPU=0,1
 POLICY_GPU_ID=0  # Which physical GPU to use (0 or 1)
 
-OUTPUTS_DIR=./eval_logs/goal_10
-TASK_SUITE=libero_goal,libero_10
-# OUTPUTS_DIR=./eval_logs/spatial_object
-# TASK=libero_spatial,libero_object
+# OUTPUTS_DIR=./eval_logs/goal_10
+# TASK_SUITE=libero_goal,libero_10
+OUTPUTS_DIR=./eval_logs/spatial_object
+TASK_SUITE=libero_spatial,libero_object # libero_spatial,libero_object,libero_goal,libero_10
 
-EPISODE=2  # run amount for each task (for success rate)
+EPISODE=10  # run amount for each task (for success rate)
 TASK_IDS='[0,1]'  # different scenes for one group
 
 # Override Docker's CUDA_VISIBLE_DEVICES to make both GPUs visible
 CUDA_VISIBLE_DEVICES=$ALL_GPU lerobot-eval  \
  --env.type=libero      \
  --env.task=$TASK_SUITE  \
- --eval.batch_size=2    \
+ --eval.batch_size=10    \
  --eval.n_episodes=$EPISODE   \
  --policy.path=lerobot/pi05_libero_finetuned    \
  --policy.n_action_steps=10 \
