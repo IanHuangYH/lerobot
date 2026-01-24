c7d38e31b1244e4f395aebde6cff2541dc9bddda
Your Name <your@email.com>
2026-01-24 13:59:25 +0000

    simplify the script for add object into env

--- pi_setting/eval/set_liberoenv_addobject.sh
+++ pi_setting/eval/set_liberoenv_addobject.sh
@@ -1,17 +1,19 @@
 
-# spatial_0
-# TASK=pick_up_the_black_bowl_between_the_plate_and_the_ramekin_and_place_it_on_the_plate
-# cd /workspace/lerobot/pi_setting/eval && python preserve_exact_positions.py \
-#     --old_bddl /workspace/lerobot/third_party/LIBERO/libero/libero/bddl_files/libero_spatial/$TASK.bddl.old \
-#     --new_bddl /workspace/lerobot/third_party/LIBERO/libero/libero/bddl_files/libero_spatial/$TASK.bddl \
-#     --old_init /workspace/lerobot/third_party/LIBERO/libero/libero/init_files/libero_spatial/$TASK.bak \
-#     --output_init /workspace/lerobot/third_party/LIBERO/libero/libero/init_files/libero_spatial/$TASK.pruned_init
+ 
+GROUP=libero_object
+TASK=pick_up_the_alphabet_soup_and_place_it_in_the_basket
+
+# spatial_0, GROUP=libero_spatial, TASK=pick_up_the_black_bowl_between_the_plate_and_the_ramekin_and_place_it_on_the_plate
+
+# spatial_1, GROUP=libero_spatial, TASK=pick_up_the_black_bowl_next_to_the_ramekin_and_place_it_on_the_plate
+
+# object_0, GROUP=libero_object, TASK=pick_up_the_alphabet_soup_and_place_it_in_the_basket
 
 
-# spatial_1
-TASK=pick_up_the_black_bowl_next_to_the_ramekin_and_place_it_on_the_plate
 cd /workspace/lerobot/pi_setting/eval && python preserve_exact_positions.py \
-    --old_bddl /workspace/lerobot/third_party/LIBERO/libero/libero/bddl_files/libero_spatial/$TASK.bddl.old \
-    --new_bddl /workspace/lerobot/third_party/LIBERO/libero/libero/bddl_files/libero_spatial/$TASK.bddl \
-    --old_init /workspace/lerobot/third_party/LIBERO/libero/libero/init_files/libero_spatial/$TASK.bak \
-    --output_init /workspace/lerobot/third_party/LIBERO/libero/libero/init_files/libero_spatial/$TASK.pruned_init
+    --old_bddl /workspace/lerobot/third_party/LIBERO/libero/libero/bddl_files/$GROUP/$TASK.bddl.old \
+    --new_bddl /workspace/lerobot/third_party/LIBERO/libero/libero/bddl_files/$GROUP/$TASK.bddl \
+    --old_init /workspace/lerobot/third_party/LIBERO/libero/libero/init_files/$GROUP/$TASK.bak \
+    --output_init /workspace/lerobot/third_party/LIBERO/libero/libero/init_files/$GROUP/$TASK.pruned_init
+
+    
